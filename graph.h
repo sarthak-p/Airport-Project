538749c1365adeb7f0d2037b287b0deedf81d9af
Carter Smith <carters3@illinois.edu>
2021-04-19 21:29:51 -0500

    updated graph

--- graph.h
+++ graph.h
@@ -2,19 +2,27 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
 struct airportNode {
-
+    string name, code;
+    double latitude, longitude;
     airportNode * next;
 };
 
-struct routeEdge {
-    string
-    
+struct adjRoutes {
+    airportNode * first;
 };
 
 class Graph {
+    public:
+        Graph();
+        void addEdge(airportNode a, airportNode b);
+        void addNode(airportNode input);
+        void removeNode(airportNode )
+    private:
+        vector<adjRoutes> adjList;
 
 };
