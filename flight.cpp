bf54236c9ee2d7224d1fc527c544caaeb3e03064
Sarthak <pusarthak@gmail.com>
2021-05-05 10:46:41 -0500

    test

--- flight.cpp
+++ flight.cpp
@@ -36,31 +36,4 @@ vector<string> Flight::dijkstra(const string &start, const string &end) {
     processed.insert(startId);
 }
 
-void Flight::airport(const string & filename, string line) {
-    ifstream airportFile(filename.c_str());
-    string name, sname, country, ID, cID;
-    string useless1, useless2, useless3, stringLat, stringLong;
 
-    while (airportFile.good()) {
-        getline(s, name, ',');
-        getline(s, sname, ',');
-        getline(s, country, ',');
-        getline(s, ID, ',');
-        getline(s, cID, ',');
-        getline(s, stringLat, ',');
-        getline(s, stringLong, ',');
-        getline(s, useless1, ',');
-        getline(s, useless2, ',');
-        getline(s, useless3, '\n');
-    }
-
-    //vector fields
-    airportNode entry(name, stoi(ID), stod(stringLat), stod(stringLong));
-    airportFile.close();
-<<<<<<< HEAD
-}
-
-
-=======
-}
->>>>>>> 871423fe3c1a68799336a195c0e2c3d3a34b06e8
