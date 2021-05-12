108303e971092ff8afde520462261b8a78fa652d
ialviar2 <ialviar2@illinois.edu>
2021-05-12 07:07:48 -0500

    finished Dijkstra

--- main.cpp
+++ main.cpp
@@ -1,72 +1,94 @@
 #include <iostream>
 #include "graph.h"
 #include "readFromFile.cpp"
-//#include "adjMatrix.h"
-//#include "dijkstra.h"
+#include "adjMatrix.h"
+#include "dijkstra.h"
 
-void sebby(); ////comment out to run DFS
-// int main() {
-//     std::cout << "This is our main file" << std::endl;
-//     sebby(); //comment out to run DFS
-//     return 0; //comment out to run DFS
-    
-//     //original main:
-//     std::cout << "This is our main file" << std::endl;
-//     std::vector<airportNode> airportVector = readAirports("data/airports.dat");
-//     std::cout << "first airportNode: " << airportVector[0].name_ << std::endl;
-//     std::cout << "second airportNode: " << airportVector[1].name_ << std::endl;
-//     std::cout << "distance: " << airportVector[0].dist(airportVector[1]) << std::endl;
+using namespace std;
 
-//     std::vector<route> routeVector = readRoutes("data/routes.dat");
-//     std::cout << "Route #1 source: " << routeVector[0].source << std::endl;
-//     std::cout << "Route #2 destination: " << routeVector[0].destination << std::endl;
+void sebby();
+string getInput(const string &question, const bool &allCaps);
+void getInput(string &startAirport, string &endAirport);
+bool checkIATAFormat(const string &code);
+void checkCapitalization(string &text);
 
-//     return 0;
-// }
-
+int main() {
+    //std::cout << "Open main file" << std::endl;
+    string startAirportCode, endAirportCode;
+    getInput(startAirportCode, endAirportCode);
+    std::vector<airportNode> airportVector = readAirports("data/airports.dat");
+    std::vector<route> routeVector = readRoutes("data/routes.dat");
+    adjMatrix myMatrix(airportVector, routeVector);
+   
+   /* Start and end airport nodes */
+    //std::cout << "first airportNode: " << myMatrix.nameToIndex["\"" + startAirportCode + "\""] << std::endl;
+    //std::cout << "second airportNode: " << myMatrix.nameToIndex["\"" + endAirportCode + "\""] << std::endl;
+    //std::cout <<"dist in matrix:"<< myMatrix.matrix[3547][3617] << std::endl;
+    dijkstra(myMatrix.matrix, myMatrix.nameToIndex["\"" + startAirportCode + "\""], myMatrix.nameToIndex["\"" + endAirportCode + "\""], myMatrix);
+    return 0; 
+}
 
 /**
 void graph_test() {
-    vector<Airportnode> a = readAirports(); 
-    vector<route> b = readRoutes(); 
-    Graph graph = Graph(a, b); 
-    graph.print(); 
+    vector<Airportnode> a = readAirports();
+    vector<route> b = readRoutes();
+    Graph graph = Graph(a, b);
+    graph.print();
 }
 **/
-//comment out to run DFS
-// void sebby() {
-//   std::cout << "This is SEBBY" << std::endl;
-//   std::vector<airportNode> airportVector = readAirports("data/airports.dat");
-//   std::cout << "Airports read from file" << std::endl;
-//   std::cout << "first airportNode: " << airportVector[0].name_ << std::endl;
-//   std::cout << "second airportNode: " << airportVector[1].name_ << std::endl;
-  
-//   std::vector<route> routeVector = readRoutes("data/routes.dat");
-//   route sampleRoute = routeVector[17314];
-  
-//   std::cout << "route vector size: " << routeVector.size() << std::endl;
-  
-//   adjMatrix myMatrix(airportVector, routeVector);
-//   std::cout << "sample of AdjMatrix (2810, 2832): " << myMatrix.matrix[2810][2832] << std::endl;
-  //myMatrix.nameToIndex["\"ORD\""]
-  /**for (int i =0; i < 100; i++) {
-    for (int j = 0; j < 100; j++) {
-      std::cout << "sample of AdjMatrix: " << myMatrix.matrix[i][j] << std::endl;
+
+// Prompt user to input start and end IATA airport codes
+void getInput(string &startAirport, string &endAirport) {
+    //std::cout << "Test getInput class" << std::endl;
+
+    bool valid = false;
+    while (!valid) {
+        startAirport = getInput("Input IATA code of start airport (ALL CAPS): ", true);
+        while (!checkIATAFormat(startAirport)) {
+          startAirport = getInput("ERROR: Improper format. Try again: ", true);
+        }
+       
+        endAirport = getInput("Input IATA code of end airport (ALL CAPS): ", true);
+        while (!checkIATAFormat(endAirport)) {
+          endAirport = getInput("ERROR: Improper format. Try again: ", true);
+        }
+
+        valid = true;
+        if (startAirport == endAirport) {
+          cout << "ERROR: Starting and ending airport are the same. Try again." << endl;
+          valid = false;
+        }
     }
-  }**/
-  
-  
-  
-//   make sample graph:
-//   Graph sG = Graph();
-//   std::cout << "Graph id: " << sG.gid << std::endl;
-//   sG.addNode(airportVector[0]);
-//   sG.addNode(airportVector[1]);
-//   sG.addEdge(sampleRoute);
-  
-//   sG.print();
-  
-  
-  //dijkstra("GKA", "MAG");
-  
-//}
+}
+
+// Get input from user
+string getInput(const string &question, const bool &allCaps) {
+  string line;
+  cout << question;
+  getline(cin, line);
+  if (allCaps) {
+    checkCapitalization(line);
+  }
+  return line;
+}
+
+// Returns true if string is in IATA format
+bool checkIATAFormat(const string &code) {
+  if (code.size() != 3) {
+    return false;
+  }
+  bool checkLetters = true;
+  for (size_t i = 0; i < code.size(); ++i) {
+    if (!isalpha(code[i])) {
+      checkLetters = false;
+    }
+  }
+  return checkLetters;
+}
+
+// Helper function to convert string to all caps if needed
+void checkCapitalization(string &text) {
+  for (size_t i = 0; i < text.size(); ++i) {
+    text[i] = toupper(text[i]);
+  }
+}
