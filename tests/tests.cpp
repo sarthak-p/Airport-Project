091a3bb5aa37d2b38680deb90b800adb1ddfe64c
Carter Smith <carters3@illinois.edu>
2021-05-12 22:20:56 -0500

    Added tests (not working)

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,6 +1,8 @@
 #include "../cs225/catch/catch.hpp"
 #include "../graph.h"
 #include "../readFromFile.cpp"
+#include "../adjMatrix.h"
+#include "../centrality.h"
 
 #include <vector>
 #include <iostream>
@@ -23,4 +25,35 @@ TEST_CASE("Succesfully parses routes", "[weight=1]") {
     route firstRoute = result[0];
     REQUIRE(firstRoute.source == "AER");
     REQUIRE(firstRoute.destination == "KZN");
+}
+
+TEST_CASE("Succesfully tests centrality @ JFK", "[weight=1'") {
+    vector<airportNode> airportVector = readAirports("data/airports.dat");
+    vector<route> routeVector = readRoutes("data/routes.dat");
+    adjMatrix testMatrix(airportVector, routeVector);
+    string source = "JFK";
+    string centrality = calcCentrality(testMatrix, source, airportVector.size());
+    REQUIRE(centrality != "Looks like you input the wrong size airport code.");
+    REQUIRE(centrality != "This airport code couldn't be found.");
+    REQUIRE(centrality == "The Normalized Degree Centrality for JFK is 0.020787.");
+}
+
+TEST_CASE("Succesfully tests centrality @ faulty input", "[weight=1'") {
+    vector<airportNode> airportVector = readAirports("data/airports.dat");
+    vector<route> routeVector = readRoutes("data/routes.dat");
+    adjMatrix testMatrix(airportVector, routeVector);
+    string source = "ORDD";
+    string centrality = calcCentrality(testMatrix, source, airportVector.size());
+    REQUIRE(centrality != "This airport code couldn't be found.");
+    REQUIRE(centrality == "Looks like you input the wrong size airport code.");
+}
+
+TEST_CASE("Succesfully tests centrality @ unknown airport", "[weight=1'") {
+    vector<airportNode> airportVector = readAirports("data/airports.dat");
+    vector<route> routeVector = readRoutes("data/routes.dat");
+    adjMatrix testMatrix(airportVector, routeVector);
+    string source = "ZZZ";
+    string centrality = calcCentrality(testMatrix, source, airportVector.size());
+    REQUIRE(centrality != "Looks like you input the wrong size airport code.");
+    REQUIRE(centrality == "This airport code couldn't be found.");
 }
